84210f42468d5608d1e0fa3f193610fc0fac5711
erikloos <erik.loos@gmx.de>
2026-04-21 22:29:53 +0200

    Refactored Code

--- app.py
+++ app.py
@@ -1,32 +1,38 @@
-from flask import Flask, render_template, request, redirect, url_for
+"""Module that runs the application"""
+
 import json
 import os
+from flask import Flask, render_template, request, redirect, url_for
+
 
 app = Flask(__name__)
 
 def load_posts():
-    """Loads JSON file"""
+    """Loads blog posts from JSON file"""
     try:
-        with open("data/blog_posts.json", "r") as handle:
+        with open("data/blog_posts.json", "r", encoding="utf-8") as handle:
             return json.load(handle)
     except (json.JSONDecodeError, FileNotFoundError):
         return []
 
 
-def save_posts(blog_posts):
-    """Save JSON file"""
+def save_posts(blog_posts: list):
+    """Save the blog posts in the JSON file"""
     os.makedirs("data", exist_ok=True)
-    with open("data/blog_posts.json", "w") as handle:
+    with open("data/blog_posts.json", "w", encoding="utf-8") as handle:
         json.dump(blog_posts, handle)
 
 @app.route('/')
 def index():
+    """Renders the index page with all posts"""
     blog_posts = load_posts()
     return render_template('index.html', blog_posts=blog_posts)
 
 
 @app.route('/add', methods= ['GET', 'POST'])
 def add():
+    """Render the add.html and add a post to the JSON file
+    with all blog posts and redirects to the index.html"""
     new_post = {}
     if request.method == 'POST':
         blog_posts = load_posts()
@@ -47,8 +53,9 @@ def add():
 
 
 @app.route('/delete/<int:post_id>')
-def delete(post_id):
-    # Find the post with the post_id and remove it from the list
+def delete(post_id: int):
+    """Delete a post with the specific 'post_id' in the JSON file.
+     Redirects to the main page with all blog posts."""
     blog_posts = load_posts()
     new_blog_posts = [post for post in blog_posts if post['id'] != post_id]
 
@@ -58,7 +65,9 @@ def delete(post_id):
 
 
 @app.route('/update/<int:post_id>', methods=['GET', 'POST'])
-def update(post_id):
+def update(post_id: int):
+    """Render the update.html and update a post in the JSON file
+    and redirects to the index.html"""
     blog_posts = load_posts()
     post = fetch_post_by_id(blog_posts, post_id)
     if post is None:
@@ -86,7 +95,8 @@ def update(post_id):
     return render_template('update.html', post=post)
 
 
-def fetch_post_by_id(blog_posts, post_id):
+def fetch_post_by_id(blog_posts: list, post_id: int):
+    """Return a post with a specific post id or None if no post with this post id was found"""
     for post in blog_posts:
         if post_id == post['id']:
             return post
