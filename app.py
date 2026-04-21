1836fc27ff4bc850ca09117e4c89d0704dd73fe0
erikloos <erik.loos@gmx.de>
2026-04-21 22:34:20 +0200

    Changed Port to standard value (5000)

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ def fetch_post_by_id(blog_posts: list, post_id: int):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
