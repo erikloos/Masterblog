30eee6893fa9cf52dd5bd36f206d2b39d02df82a
erikloos <erik.loos@gmx.de>
2026-04-21 20:00:40 +0200

    Added function to add a post and update the json file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -8,11 +8,30 @@ def load_posts(file_path:str):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-blog_posts = load_posts("data/blog_posts.json")
 
 @app.route('/')
 def index():
+    blog_posts = load_posts("data/blog_posts.json")
     return render_template('index.html', blog_posts=blog_posts)
+
+
+@app.route('/add', methods= ['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        blog_posts = load_posts("data/blog_posts.json")
+        new_post = {}
+
+        new_post["id"] = len(blog_posts) + 1
+        new_post["author"] = request.form["author"]
+        new_post["title"] = request.form["title"]
+        new_post["content"] = request.form["content"]
+        blog_posts.append(new_post)
+
+        with open("data/blog_posts.json", "w") as handle:
+            json.dump(blog_posts, handle)
+
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
