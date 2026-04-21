f9ecb07a1e70d6128be6b603978f57fc703593ed
erikloos <erik.loos@gmx.de>
2026-04-21 17:19:24 +0200

    Added html template, stylesheet and json file. Updated the template and app.py to load data from the json file.

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+def load_posts(file_path:str):
+    """Loads JSON file"""
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+blog_posts = load_posts("data/blog_posts.json")
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html', blog_posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
