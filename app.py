ffba8749a0c1809b7f02d7af1810ea9e89a56b5b
erikloos <erik.loos@gmx.de>
2026-04-21 22:10:02 +0200

    Added the function to update and delete a blog post

--- app.py
+++ app.py
@@ -1,37 +1,96 @@
 from flask import Flask, render_template, request, redirect, url_for
 import json
+import os
 
 app = Flask(__name__)
 
-def load_posts(file_path:str):
+def load_posts():
     """Loads JSON file"""
-    with open(file_path, "r") as handle:
-        return json.load(handle)
+    try:
+        with open("data/blog_posts.json", "r") as handle:
+            return json.load(handle)
+    except (json.JSONDecodeError, FileNotFoundError):
+        return []
 
+
+def save_posts(blog_posts):
+    """Save JSON file"""
+    os.makedirs("data", exist_ok=True)
+    with open("data/blog_posts.json", "w") as handle:
+        json.dump(blog_posts, handle)
 
 @app.route('/')
 def index():
-    blog_posts = load_posts("data/blog_posts.json")
+    blog_posts = load_posts()
     return render_template('index.html', blog_posts=blog_posts)
 
 
 @app.route('/add', methods= ['GET', 'POST'])
 def add():
+    new_post = {}
     if request.method == 'POST':
-        blog_posts = load_posts("data/blog_posts.json")
-        new_post = {}
+        blog_posts = load_posts()
+        if len(blog_posts) == 0:
+            new_post["id"] = 1
+        else:
+            new_post["id"] = max(post["id"] for post in blog_posts) + 1
 
-        new_post["id"] = len(blog_posts) + 1
         new_post["author"] = request.form["author"]
         new_post["title"] = request.form["title"]
         new_post["content"] = request.form["content"]
         blog_posts.append(new_post)
 
-        with open("data/blog_posts.json", "w") as handle:
-            json.dump(blog_posts, handle)
-
+        save_posts(blog_posts)
         return redirect(url_for('index'))
+
     return render_template('add.html')
+
+
+@app.route('/delete/<int:post_id>')
+def delete(post_id):
+    # Find the post with the post_id and remove it from the list
+    blog_posts = load_posts()
+    new_blog_posts = [post for post in blog_posts if post['id'] != post_id]
+
+    save_posts(new_blog_posts)
+
+    return redirect(url_for('index'))
+
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    blog_posts = load_posts()
+    post = fetch_post_by_id(blog_posts, post_id)
+    if post is None:
+        # Post not found
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        updated_post = {
+        "id": post_id,
+        "author": request.form["author"],
+        "title": request.form["title"],
+        "content": request.form["content"]
+        }
+
+        new_blog_posts = []
+        for post in blog_posts:
+            if post_id == post['id']:
+                new_blog_posts.append(updated_post)
+            else:
+                new_blog_posts.append(post)
+
+        save_posts(new_blog_posts)
+        return redirect(url_for('index'))
+
+    return render_template('update.html', post=post)
+
+
+def fetch_post_by_id(blog_posts, post_id):
+    for post in blog_posts:
+        if post_id == post['id']:
+            return post
+    return None
 
 
 if __name__ == '__main__':
